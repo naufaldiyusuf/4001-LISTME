5e3d6e0e0e5a9dc26e51dfa6a3b47dae364b8df0
Your Name <you@example.com>
2019-02-28 17:02:22 +0700

    Push

--- app/src/main/java/android/example/listme/Pemasukkan.java
+++ app/src/main/java/android/example/listme/Pemasukkan.java
@@ -10,7 +10,5 @@ public class Pemasukkan extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pemasukkan);
     }
+    //commend push
 }
-//PUSHNYA ADILLA
-
-//testing push
