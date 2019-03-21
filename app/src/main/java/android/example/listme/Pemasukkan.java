1e604dcb72ab79d917f7ff68a6b46d3525aa4564
naufaldiyusuf <naufaldi.yusuf@gmail.com>
2019-03-21 11:41:12 +0800

    commit fungsi pemasukan

--- app/src/main/java/android/example/listme/Pemasukkan.java
+++ app/src/main/java/android/example/listme/Pemasukkan.java
@@ -1,14 +1,30 @@
 package android.example.listme;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Pemasukkan extends AppCompatActivity {
+
+    TextView bulan, pemasukan, note;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pemasukkan);
+
+        bulan = (TextView) findViewById(R.id.isiNotePemasukkan_editText);
+        pemasukan = (TextView) findViewById(R.id.isiJmlPemasuk_editText);
+        note = (TextView) findViewById(R.id.isiNotePemasukkan_editText);
+
+        Intent tes2 = new Intent(Pemasukkan.this, MainActivity.class);
+        startActivity(tes2);
+        tes2.putExtra("bulan", "pemasukan1");
+        tes2.putExtra("pemasukan", "pemasukan2");
+        tes2.putExtra("note", "pemasukan3");
+
     }
     //commend push
     //error
