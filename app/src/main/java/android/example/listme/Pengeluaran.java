00f744c3c0c53aeca84133b0f3c045ec735b2011
naufaldiyusuf <47377952+naufaldiyusuf@users.noreply.github.com>
2019-03-21 15:27:12 +0700

    Revert "Revert "this commit will close #3""

--- app/src/main/java/android/example/listme/Pengeluaran.java
+++ app/src/main/java/android/example/listme/Pengeluaran.java
@@ -1,13 +1,24 @@
 package android.example.listme;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Pengeluaran extends AppCompatActivity {
+
+    TextView bulan, pengeluaran;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pengeluaran);
+    bulan = (TextView) findViewById(R.id.isiBulanPengeluaraan_editText);
+    pengeluaran = (TextView) findViewById(R.id.isiJmlPengeluaran_editText);
+
+    Intent tes3 = new Intent(Pengeluaran.this, MainActivity.class);
+        startActivity(tes3);
+        tes3.putExtra("bulan", "pengeluaran1");
+        tes3.putExtra("pengeluaran", "pengeluaran2");
     }
 }
