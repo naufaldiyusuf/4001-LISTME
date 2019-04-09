0461c3accd1b966ad8b4d27d0b7d0d185438d21c
naufaldiyusuf <naufaldi.yusuf@gmail.com>
2019-04-09 17:11:19 +0800

    commit login #10

---
+++ app/src/main/java/android/example/listme/login.java
@@ -0,0 +1,68 @@
+package android.example.listme;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class login extends AppCompatActivity {
+    TextView register;
+    EditText email, password;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        if (FirebaseAuth.getInstance().getCurrentUser() != null){
+            startActivity(new Intent(login.this, MainActivity.class));
+            finish();
+        }
+
+        register = (TextView) findViewById(R.id.register);
+        email = (EditText) findViewById(R.id.email);
+        password = (EditText) findViewById(R.id.password);
+        button = (Button) findViewById(R.id.login);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (email.getText().toString().isEmpty()) {
+                    return;
+                }
+                if (password.getText().toString().isEmpty()) {
+                    return;
+                }
+                if (password.getText().toString().length() <6){
+                    return;
+                }
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(email.getText().toString().trim(),password.getText().toString().trim()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        startActivity(new Intent(login.this, MainActivity.class));
+                        finish();
+                    }
+                });
+
+
+            }
+        });
+
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(login.this, RegisterActivity.class));
+            }
+        });
+
+    }
+}
