a9d5635cdaaf9cfeb17522b4fed024eb95cc0cb0
greenhard <greenhardsi@gmail.com>
2019-04-11 16:45:07 +0700

    JSON

--- app/src/main/java/android/example/listme/MainActivity.java
+++ app/src/main/java/android/example/listme/MainActivity.java
@@ -1,7 +1,7 @@
 package android.example.listme;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
